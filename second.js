31372b40c787d6639f2ff2032fa18b6af6d8ae02
aakanksha-7 <135256053+aakanksha-7@users.noreply.github.com>
2025-10-05 00:23:42 +0545

    Add files via upload

---
+++ second.js
@@ -0,0 +1,47 @@
+// // Level 2: Working with calculations
+
+// // Print the sum of numbers from 1 to 10.
+// let sum = 0;
+// for (let i = 1; i <= 10; i++) {
+//    sum += i;
+//    console.log(sum);
+    
+// }
+// Print the factorial of a number (like 5! = 120).
+// for (let i = 1; i <= n ; i++) {
+//     fact *= i
+//     console.log(fact);
+    
+// }
+
+// let n = 5;
+// let fact = 1;
+// let i = 1;
+// while (i <= n) {
+//     fact *= i;
+//     i++;
+// };
+// console.log(fact);  // 120
+// Print the squares of numbers from 1 to 10.
+
+for (let i = 1; i <= 10; i++) {
+   let num = i ** 2 ;
+   console.log(num);
+    
+}
+
+// Print the cubes of numbers from 1 to 5.
+for (let i = 1; i <= 10; i++) {
+   let num = i ** 3 ;
+   console.log(num);
+    
+}
+
+// Print all numbers between 1 and 50 that are divisible by 7.
+
+for (let i = 1; i <= 50; i++) {
+    if (i % 7 == 0) {
+        console.log(i);
+    };
+    
+}
